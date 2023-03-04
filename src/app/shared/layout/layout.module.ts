598c2b62a93501db949c45584c58c9cbd4ad21d9
Alberto Moreno <alberto.moreno2357@gmail.com>
2023-03-04 12:27:47 +0100

    feat: add layout components & module

---
+++ src/app/shared/layout/layout.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../modules/material.module';
+
+// Components
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
+  exports: [
+    FooterComponent, HeaderComponent, NavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ]
+})
+export class LayoutModule { }
